d2909144066c9d9d1c03624122edeaa4fdd000ed
Ramkumar K <160816749+itmerk@users.noreply.github.com>
2024-08-03 15:22:46 +0530

    Add new app page

--- techblog/views.py
+++ techblog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Test Page. Your blog index pages.")
+
+def detial(request):
+    return HttpResponse("Test Page. Your Detial page.")
