7d84b50fc2f93b1bb818f0f31050cd370dcbe791
Ramkumar K <160816749+itmerk@users.noreply.github.com>
2024-08-04 10:11:50 +0530

    Add files via upload

--- techblog/urls.py
+++ techblog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'techblog'
+
 urlpatterns = [
     path("", views.index, name ="index"),
     path("post/<str:post_id>",views.detial, name = "detial"),
-    path("new_url",views.new_url_view, name = "new_url"),
+    path("new_something_url",views.new_url_view, name = "new_page_url"),
     path("old_url",views.old_url_redirect, name = "old_url")
-
 ]
--- techblog/views.py
+++ techblog/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse,include
 
 # Create your views here.
 def index(request):
-    return render(request,'techblog/index.html')
+    techblog_title = "Latest Tech updates"
+    posts = [
+        {'id':1,'title':'Post 1', 'content': 'Content of post 1'},
+        {'id':2,'title':'Post 2', 'content': 'Content of post 2'},
+        {'id':3,'title':'Post 3', 'content': 'Content of post 3'},
+        {'id':4,'title':'Post 4', 'content': 'Content of post 4'},
+        {'id':5,'title':'Post 5', 'content': 'Content of post 5'},
+        {'id':6,'title':'Post 6', 'content': 'Content of post 6'}
+    ]
+    return render(request,'techblog/index.html',{"techblog_title":techblog_title, 'posts': posts})
 
 def detial(request,post_id):
     return render(request,'techblog/detial.html')
 
 def old_url_redirect(request):
-    return redirect("new_url")
+    return redirect(reverse('techblog:new_page_url'))
 
 def new_url_view(request):
     return HttpResponse("This is new url")
